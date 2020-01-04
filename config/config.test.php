c03778fa28c86910b42c49c6b266d2b6c00a8344
Viorel Robu <wwjd.vio@gmail.com>
2020-01-04 22:27:36 +0200

    TDD

---
+++ config/config.test.php
@@ -0,0 +1,8 @@
+<?php
+
+// set the database connection credentials
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'framework');
+define('DB_USER', 'root');
+define('DB_PASS', '');
--- tests/DBConnectionTest.php
+++ tests/DBConnectionTest.php
@@ -1,13 +1,36 @@
 <?php
 
+use Phramework\App\Kernel;
 use PHPUnit\Framework\TestCase;
+use Phramework\Core\Database\DatabaseConnection;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/helpers.php';
+
+$template = [
+    '__@' => '<?php',
+    '@__' => '?>'
+];
+
+Kernel::bind('config', require __DIR__ . '/../config/config.test.php');
+Kernel::bind('connection', DatabaseConnection::connect(DB_HOST, DB_NAME, DB_USER, DB_PASS));
+Kernel::bind('template', $template);
 
 class DBConnectionTest extends TestCase 
 {
     /** @test */
+    public function configuration_exists_and_is_not_null()
+    {
+        $this->assertNotNull(DB_HOST);
+        $this->assertNotNull(DB_NAME);
+        $this->assertNotNull(DB_USER);
+        $this->assertNotNull(DB_PASS);
+    }
+    
+    /** @test */
     public function app_can_connect_to_database()
     {
-        $app = [];
-        $this->assertEmpty($app);
+        $db = DatabaseConnection::connect(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+        $this->assertInstanceOf(PDO::class, $db);
     }
 }
